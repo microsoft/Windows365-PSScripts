5e63bb1d163f55cc1da2996ef1a8146baa2b8825
Donna Ryan <100233767+DonnaRyanMicrosoft@users.noreply.github.com>
2025-10-12 20:38:50 -0400

    Logging enabled + error handling
    Added logging module,
    Added error handling
    Added rename MSIX download and rename to original file name.

--- Windows App Installer MultiTool/Windows App Installer.ps1
+++ Windows App Installer MultiTool/Windows App Installer.ps1
@@ -1,8 +1,8 @@
 ﻿#cls
 #Needs:
 #try/catch in main functions
-#change write-host to proper logging
-#Test Param Block
+#complete #change update-log -Data to proper logging
+#complete #Test Param Block
 #test with store disabled from policy
 #test on Windows 10
 #test on LTSC greater than 1809
@@ -17,12 +17,10 @@ Param(
     [int]$DisableAutoUpdate = 0,
     [parameter(mandatory = $false, HelpMessage = "Value to set auto update reg key")]
     [ValidateSet($true,$false)]
-    [string]$UninstallMSRDC = $true
+    [string]$UninstallMSRDC = $true,
+    [parameter(mandatory = $false, HelpMessage = "Log path and file name")] 
+    [string]$logpath = "$env:windir\temp\MultiTool.log"
 )
-
-
-#$source = "Store" #Store,WinGet,MSIX,All
-#$UninstallMSRDC = $true #$true,$false
 
 #$DisableAutoUpdate = 0 
 #0: Enables updates (default value)
@@ -30,20 +28,54 @@ Param(
 #2: Disable updates from the Microsoft Store
 #3: Disable updates from the CDN location
 
+#logging function
+function update-log {
+    Param(
+        [Parameter(
+            Mandatory = $true, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0
+        )]
+        [string]$Data,
+        [validateset('Information', 'Warning', 'Error', 'Comment')]
+        [string]$Class = "Information",
+        [validateset('Console', 'File', 'Both')]
+        [string]$Output 
+    )
+
+    $date = get-date -UFormat "%m/%d/%y %r"
+    $String = $Class + " " + $date + " " + $data
+    if ($Output -eq "Console") { Write-Output $string | out-host }
+    if ($Output -eq "file") { Write-Output $String | out-file -FilePath $logpath -Append }
+    if ($Output -eq "Both") {
+        Write-Output $string | out-host
+        Write-Output $String | out-file -FilePath $logpath -Append
+    }
+}
+
 #function to uninstall MSRDC by pulling MSIEXEC.EXE GUID from the registy - primary method
 function uninstall-MSRDCreg{
     
     $MSRCDreg = Get-ItemProperty hklm:\software\microsoft\windows\currentversion\uninstall\* | Where-Object {$_.Displayname -like "*Remote Desktop*"} | Select-Object DisplayName,DisplayVersion,UninstallString,QuietUninstallString
     if ($MSRCDreg.DisplayName -eq "Remote Desktop"){
-        write-host "Remote Desktop Installation Found"
-        #write-host "Version " $MSRCDreg.DisplayVersion
+        update-log -Data "Remote Desktop Installation Found" -Class Information -Output Both
         $uninstall = $MSRCDreg.uninstallstring -replace "MsiExec.exe /X",""
-        write-host "Uninstalling Remote Desktop"
-        Start-Process -FilePath "msiexec.exe" -ArgumentList "/x $($uninstall) /q /norestart"
+        update-log -Data "Uninstalling Remote Desktop"  -Class Information -Output Both
+        
+        try{
+            Start-Process -FilePath "msiexec.exe" -ArgumentList "/x $($uninstall) /q /norestart" -ErrorAction Stop
+        }
+        catch
+        {
+            update-log -Data "Something went wrong uninstalling Remote Desktop" -Class Error -Output Both 
+            Update-Log -data $_.Exception.Message -Class Error -Output Both
+        }
+    
     }
     else
     {
-    write-host "Remote Desktop not detected via registry. Trying as package"
+    update-log -Data "Remote Desktop not detected via registry. Trying as package" -Class Information -Output Both
     uninstall-MSRDC
     }
 }
@@ -51,91 +83,152 @@ function uninstall-MSRDCreg{
 #Function to uninstall MSRDC via uninstall-package as a secondary method
 function uninstall-MSRDC{
     try{
-        write-host "Looking to see if Remote Desktop is an installed package"
-        $MSRDC = Get-Package -Name "Remote Desktop" -ErrorAction Stop
+        update-log -Data "Looking to see if Remote Desktop is an installed package" -Class Information -Output Both
+        try{
+            $MSRDC = Get-Package -Name "Remote Desktop" -ErrorAction Stop
+        }
+        catch
+        {
+            Update-Log -data $_.Exception.Message -Class Error -Output Both
+        }
 
         if ($MSRDC.name -eq "Remote Desktop"){
-           write-host "Remote Desktop Install Found"
-           #write-host "Version: " $MSRDC.Version
-           write-host "Uninstalling Remote Desktop"
-           Uninstall-Package -Name "Remote Desktop" -force | Out-Null
-           write-host "Remote Desktop uninstalled"
+           update-log -Data "Remote Desktop Install Found" -Class Information -Output Both
+           #update-log -Data "Version: " $MSRDC.Version
+           update-log -Data "Uninstalling Remote Desktop" -Class Information -Output Both
+           try{
+                Uninstall-Package -Name "Remote Desktop" -force -ErrorAction Stop| Out-Null
+           }
+           catch
+           {
+                Update-Log -data $_.Exception.Message -Class Error -Output Both
+           }
+         
+           update-log -Data "Remote Desktop uninstalled" -Class Information -Output Both
        }
     }
     catch
     {
-        Write-Host "Remote Desktop not found as package."
+        update-log -Data "Remote Desktop not found as package." -Class Information -Output Both
     }
 }
 
 #function to install Windows App from MS Store - write install process log to $env:windir\temp\WindowsAppStoreInstall.log
 function install-windowsappstore{
-   invoke-command -ScriptBlock { winget install 9N1F85V9T8BN --accept-package-agreements --accept-source-agreements} | Out-File -FilePath c:\windows\temp\WindowsAppStoreInstall.log -Append #MS Store Install 
+   update-log -Data "Writing install process log to $env:windir\temp\WindowsAppStoreInstall.log" -Class Information -Output Both
+   try{
+        invoke-command -ScriptBlock { winget install 9N1F85V9T8BN --accept-package-agreements --accept-source-agreements} | Out-File -FilePath $env:windir\temp\WindowsAppStoreInstall.log -Append #MS Store Install 
+   }
+   catch
+   {
+        Update-Log -data $_.Exception.Message -Class Error -Output Both
+        Exit 1
+   }
+
 }
 
 #Function to install Windows App from Winget CDN - write install process log to $env:windir\temp\WindowsAppWinGetInstall.log
 function install-windowsappwinget{
-    invoke-command -ScriptBlock {winget install Microsoft.WindowsApp --accept-package-agreements --accept-source-agreements} | Out-File -FilePath c:\windows\temp\WindowsAppWinGetInstall.log -Append #Winget Install
+    update-log -Data "Writing install process log to $env:windir\temp\WindowsAppWinGetInstall.log" -Class Information -Output Both
+    try{
+        invoke-command -ScriptBlock {winget install Microsoft.WindowsApp --accept-package-agreements --accept-source-agreements} | Out-File -FilePath $env:windir\temp\WindowsAppWinGetInstall.log -Append #Winget Install
+    }
+    catch
+    {
+        Update-Log -data $_.Exception.Message -Class Error -Output Both
+        Exit 1
+    }
+
 }
 
 #Function to install Windows App from MSIX direct download
 function install-windowsappMSIX{
-    Invoke-WebRequest -uri "https://go.microsoft.com/fwlink/?linkid=2262633" -outfile "c:\windows\temp\WindowsApp.MSIX" -UseBasicParsing -PassThru #windowsapp download
-    Add-AppxPackage -Path C:\windows\temp\WindowsApp.MSIX
+
+    try{
+        if ((test-path -Path $env:windir\Temp\WindowsApp.msix) -eq $true){Remove-Item -Path $env:windir\Temp\WindowsApp.msix -Force -ErrorAction Stop}        
+        
+        $Payload = Invoke-WebRequest -uri "https://go.microsoft.com/fwlink/?linkid=2262633" -UseBasicParsing -OutFile $env:windir\Temp\WindowsApp.msix -PassThru -ErrorAction Stop
+        $filename = ($Payload.BaseResponse.ResponseUri.AbsolutePath -replace ".*/")
+    
+        if ((test-path -Path $env:windir\Temp\$filename) -eq $true){Remove-Item -Path $env:windir\Temp\$filename -Force -ErrorAction Stop}    
+    
+        Rename-Item -Path $env:windir\Temp\WindowsApp.msix -NewName $filename -Force -ErrorAction Stop
+        update-log -Data "Downloaded $filename to $env:windir\temp" -Class Information -Output Both
+        }
+    catch{
+        Update-Log -data $_.Exception.Message -Class Error -Output Both
+    }
+
+    try{
+        Add-AppxPackage -Path $env:windir\temp\$filename -ErrorAction Stop
+    }
+    catch{
+        Update-Log -data $_.Exception.Message -Class Error -Output Both
+    }
+    
+    
     }
 
 #Function to check if Windows App is installed
 function invoke-WAInstallCheck{
     if ((($testWA = get-appxpackage -name MicrosoftCorporationII.Windows365).name) -eq "MicrosoftCorporationII.Windows365"  ){
-        Write-Host "Windows App Installation found."
+        update-log -Data "Windows App Installation found." -Class Information -Output Both
         Return 0
     }
     else
     {
-        write-host "Windows App installation not found."
+        update-log -Data "Windows App installation not found." -Class Information -Output Both
         Return 1
     }
 }
 
 #function to set the registry key to control auto updates
 function invoke-disableautoupdate($num){
-    write-host "Setting disableautoupdate reg key"
+    update-log -Data "Setting disableautoupdate reg key" -Class Information -Output Both
     $path = "HKLM:\SOFTWARE\Microsoft\WindowsApp"
     If (!(Test-Path $path)) {
         New-Item -Path $path -Force
 }
-    New-ItemProperty -Path $path -Name DisableAutomaticUpdates -PropertyType DWORD -Value $num -Force    
+    try{
+        New-ItemProperty -Path $path -Name DisableAutomaticUpdates -PropertyType DWORD -Value $num -Force -ErrorAction Stop| Out-Null    
+    }
+    catch{
+        Update-Log -data $_.Exception.Message -Class Error -Output Both
+    }
+
+
 }
 
 #check if Windows App is installed. If so, skip installation. Else, install
 if ((invoke-WAInstallCheck) -eq 0){
-    write-host "Skipping Windows App Installation"
+    update-log -Data "Skipping Windows App Installation" -Class Information -Output Both
     }
 else
     {
     if ($source -eq "Store"){
-        write-host "Starting Windows App installation from Microsoft Store"
+        update-log -Data "Starting Windows App installation from Microsoft Store" -Class Information -Output Both
         install-windowsappstore
         }
     if ($source -eq "WinGet"){
-        write-host "Starting Windows App installation from WinGet"
+        update-log -Data "Starting Windows App installation from WinGet" -Class Information -Output Both
         install-windowsappwinget
         }
     if ($source -eq "MSIX"){
-        write-host "Starting Windows App installation from MSIX download"
+        update-log -Data "Starting Windows App installation from MSIX download" -Class Information -Output Both
         install-windowsappMSIX
         }
 }
 
 #verify if Windows App has now been installed. If so, move to uninstalling MSRDC. Else, fail.
 if ((invoke-WAInstallCheck) -eq 0){
-    write-host "Validated Windows App Installed"
+    update-log -Data "Validated Windows App Installed" -Class Information -Output Both
     if ($UninstallMSRDC -eq $true){uninstall-MSRDCreg}
-    #write-host "Installation Complete"
+    #update-log -Data "Installation Complete"
     }
     else
     {
-    write-host "Windows App does not appear to be installed. Something went wrong"
+    update-log -Data "Windows App does not appear to be installed. Something went wrong" -Class Error -Output Both
+    exit 1
     }
 
 #Apply auto update registry key if option selected
@@ -145,4 +238,5 @@ if ($DisableAutoUpdate -ne 0){
     if ($DisableAutoUpdate -eq 3){invoke-disableautoupdate -num 3}
 
 }
-write-host "Installation Complete"
+update-log -Data "Installation Complete" -Class Information -Output Both
+update-log -data "************" -Class Information -Output File
